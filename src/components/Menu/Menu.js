d67677edc37b27fd4f77ce626193bdfd96802143
margarita-tsaruk <margarita.tsaruk@yandex.ru>
2022-10-27 20:58:36 +0200

    diploma: frontend - fixed Menu, ready for review

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -8,21 +8,23 @@ function Menu ( { isMenuOpen, onChangeMenu } ) {
         <button className="menu__button-close" type="button" onClick={ onChangeMenu }></button>
         <Switch>
           <Route path="/">
-            <nav className="menu__nav">
-              <Link className="menu__nav__link menu__nav__link_main" to="/" onClick={ onChangeMenu }>
-                Главная
-              </Link>
-              <Link className="menu__nav__link menu__nav__link_movies" to="/movies" onClick={ onChangeMenu }>
-                Фильмы
-              </Link>
-              <Link className="menu__nav__link menu__nav__link_saved-movies" to="/saved-movies" onClick={ onChangeMenu }>
-                Сохранённые фильмы
-              </Link>
+            <div className="menu__content">
+              <nav className="menu__nav">
+                <Link className="menu__nav__link menu__nav__link_main" to="/" onClick={ onChangeMenu }>
+                  Главная
+                </Link>
+                <Link className="menu__nav__link menu__nav__link_movies" to="/movies" onClick={ onChangeMenu }>
+                  Фильмы
+                </Link>
+                <Link className="menu__nav__link menu__nav__link_saved-movies" to="/saved-movies" onClick={ onChangeMenu }>
+                  Сохранённые фильмы
+                </Link>
+              </nav> 
               <Link className="menu__nav__profile" to="/profile" onClick={ onChangeMenu }>
                 <p className="menu__nav__account">Аккаунт</p>
                 <div className="menu__nav__icon"></div>
               </Link>
-            </nav>
+            </div>
           </Route>
         </Switch>
       </div>
